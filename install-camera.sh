b807f266ab25815031e5be37a64a1fd9645e1153
Maxence A <44868381+Abela-Things@users.noreply.github.com>
2023-01-16 07:35:56 +0100

    [ADD] enable camera service before starting up.

--- install-camera.sh
+++ install-camera.sh
@@ -6,4 +6,5 @@ cd v4l2rtspserver/
 cmake . && make -j4
 sudo make install
 sudo cp rtsp-camera.service /etc/systemd/system/rtsp-camera.service
+sudo systemctl enable rtsp-camera
 sudo systemctl start rtsp-camera
