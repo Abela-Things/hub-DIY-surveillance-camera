b08af402d61a3c855eb26f2019aa83489cc340dd
Maxence Abela <maxence.abela@epitech.eu>
2023-01-28 15:59:01 +0100

    [FIX] Crontab addition was missing a user definition.

--- install-server.sh
+++ install-server.sh
@@ -1,5 +1,5 @@
 sudo apt install ffmpeg
 sudo cp server-recorder.service /etc/systemd/system/server-recorder.service
-echo '0 0 * * * /usr/bin/find /mnt/hdd1/record -name "camera*" -type f -mtime +2 -exec rm -f {} \;' | sudo tee --append /etc/crontab
+echo '0 * * * * root /usr/bin/find /mnt/hdd1/record -name "camera*" -type f -mtime +2 -exec rm -f {} \;' | sudo tee --append /etc/crontab
 sudo systemctl enable server-recorder.service
 sudo systemctl start server-recorder.service
